049d2cc0d9ac3d5dd3310ba2cc7c6649437cff39
Danyihza <55189649+Danyihza@users.noreply.github.com>
2021-08-30 22:42:08 +0700

    db

--- db_pkm.sql
+++ db_pkm.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.4
+-- version 5.0.3
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 22 Agu 2021 pada 14.49
--- Versi server: 10.4.14-MariaDB
--- Versi PHP: 7.4.11
+-- Generation Time: Aug 30, 2021 at 05:35 PM
+-- Server version: 10.4.14-MariaDB
+-- PHP Version: 7.4.11
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,7 +24,38 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `tb_user`
+-- Table structure for table `tb_artikel`
+--
+
+CREATE TABLE `tb_artikel` (
+  `id_artikel` int(11) NOT NULL,
+  `judul_artikel` text NOT NULL,
+  `isi_artikel` text NOT NULL,
+  `gambar_artikel` varchar(256) NOT NULL,
+  `artikel_utama` enum('YES','NO') NOT NULL DEFAULT 'NO',
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_otp`
+--
+
+CREATE TABLE `tb_otp` (
+  `id` int(11) NOT NULL,
+  `no_hp` varchar(14) NOT NULL,
+  `otp` varchar(6) NOT NULL,
+  `otp_updated_at` timestamp NULL DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_user`
 --
 
 CREATE TABLE `tb_user` (
@@ -32,20 +63,57 @@ CREATE TABLE `tb_user` (
   `nama_user` varchar(50) NOT NULL,
   `no_wa` varchar(15) NOT NULL,
   `alamat` text NOT NULL,
-  `jenis_kelamin` enum('Laki-Laki','Perempuan','','') NOT NULL,
+  `jenis_kelamin` enum('Laki-Laki','Perempuan') NOT NULL,
+  `otp` int(6) DEFAULT NULL,
+  `otp_updated_at` timestamp NULL DEFAULT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tb_user`
+--
+
+INSERT INTO `tb_user` (`id_user`, `nama_user`, `no_wa`, `alamat`, `jenis_kelamin`, `otp`, `otp_updated_at`, `created_at`, `updated_at`) VALUES
+('asdadasdasdasd', 'Dany Ah', '082331147549', 'asdadasdas', 'Laki-Laki', 234456, NULL, '2021-08-30 14:19:45', '2021-08-30 14:19:45');
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indeks untuk tabel `tb_user`
+-- Indexes for table `tb_artikel`
+--
+ALTER TABLE `tb_artikel`
+  ADD PRIMARY KEY (`id_artikel`);
+
+--
+-- Indexes for table `tb_otp`
+--
+ALTER TABLE `tb_otp`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tb_user`
 --
 ALTER TABLE `tb_user`
   ADD PRIMARY KEY (`id_user`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `tb_artikel`
+--
+ALTER TABLE `tb_artikel`
+  MODIFY `id_artikel` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tb_otp`
+--
+ALTER TABLE `tb_otp`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
